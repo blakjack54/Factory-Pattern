e3681d71fc96f20fda7806506209f48909eff26f
Blakjack54 <blakewalters53@gmail.com>
2024-08-07 19:45:08 -0400

    added classes

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -1,10 +1,71 @@
-﻿namespace FactoryPattern
+﻿using System;
+
+namespace FactoryPattern
 {
+    public interface IVehicle
+    {
+        void Drive();
+    }
+
+    public class Car : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Building a new Car!");
+        }
+    }
+
+    public class Motorcycle : IVehicle
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Building a new Motorcycle!");
+        }
+    }
+
+    public static class VehicleFactory
+    {
+        public static IVehicle GetVehicle(int numberOfTires)
+        {
+            switch (numberOfTires)
+            {
+                case 2:
+                    return new Motorcycle();
+                case 4:
+                    return new Car();
+                // Add more cases for different vehicles if needed
+                default:
+                    throw new ArgumentException("Invalid number of tires");
+            }
+        }
+    }
+
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of tires on the vehicle:");
+
+            // Get the number of tires from the user's input
+            if (int.TryParse(Console.ReadLine(), out int numberOfTires))
+            {
+                try
+                {
+                    // Get the vehicle from the factory
+                    IVehicle vehicle = VehicleFactory.GetVehicle(numberOfTires);
+
+                    // Call the Drive method
+                    vehicle.Drive();
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
         }
     }
 }
